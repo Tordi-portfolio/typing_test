694e639946adcf43eef4d9d717927d3b02968813
Tordi-portfolio <164408531+Tordi-portfolio@users.noreply.github.com>
2026-06-12 11:51:10 +0100

    Add files via upload

--- typingapp/settings.py
+++ typingapp/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-7xthgv^_)74xj^)ug-2skgmo6ididh5%f^=trkc4eqxzdborff
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['typing-test-0eqg.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
